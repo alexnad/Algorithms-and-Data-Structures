827f321a7ce7b006048195d6fb67fc3968b225c2
alexnad <alexandernadjarian@gmail.com>
2016-03-13 23:58:04 +0200

    add insert method

--- treap.hs
+++ treap.hs
@@ -1,23 +1,24 @@
 
-data Tree k = Empty | Node Int k (Tree k) (Tree k) deriving (Show)
+data Tree a = Empty | Node Int a (Tree a) (Tree a) deriving (Show)
 
 
-key :: Tree -> a
+key :: Tree a -> a
 key (Node _ k _ _ ) = k
 
 
-priority :: Tree -> a
+priority :: Tree a -> Int
 priority (Node p _ _ _) = p
 
-left :: Tree -> a
+
+left :: Tree a -> Tree a
 left (Node _ _ l _) = l
 
 
-right :: Tree -> a
+right :: Tree a -> Tree a
 right (Node _ _ _ r) = r
 
 
-find :: (Eq a, Ord a)  => Tree k a -> a -> Bool
+find :: (Eq a, Ord a)  => Tree a -> a -> Bool
 find Empty element = False
 find (Node p key left right) element
     | element == key = True
@@ -25,6 +26,7 @@ find (Node p key left right) element
     | element < key = find left element
 
 
+rotateLeft :: Tree a -> Tree a
 rotateLeft node = Node sonP sonKey sonLeft parent
     where
         son = (left parent)
@@ -34,7 +36,8 @@ rotateLeft node = Node sonP sonKey sonLeft parent
         parent = Node (priority node) (key node) (right son) (right node)
 
 
-rotateLeft node = Node sonP sonKey parent sonRight
+rotateRight :: Tree a -> Tree a
+rotateRight node = Node sonP sonKey parent sonRight
     where
         son = (left parent)
         sonP = priority son
@@ -43,3 +46,20 @@ rotateLeft node = Node sonP sonKey parent sonRight
         parent = Node (priority node) (key node) (left node) sonRight
 
 
+insert :: (Ord a, Eq a) => Tree a -> a -> Int -> Tree a
+insert root value p
+    | value < (key root) =
+        if p < (priority root)
+         then
+            rotateLeft insertLeft
+         else
+            insertLeft
+    | value > (key root) = 
+        if p < (priority root)
+         then
+            rotateLeft insertRight
+         else
+            insertRight
+    where
+        insertLeft = Node (priority root) (key root) (insert (left root) value p) (right root)
+        insertRight = Node (priority root) (key root) (left root) (insert (right root) value p)
